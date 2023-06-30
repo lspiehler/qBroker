6b5210877174f183c86c9c422e50354a066316ef
Lyas Spiehler <lspiehler@gmail.com>
2023-06-30 20:10:21 +0000

    testing print mapping protocol

---
+++ api/views/mapping_test.php
@@ -0,0 +1,8 @@
+<a href="printer://2E-Lsr-03@lcmc-prtsrv02.lcmchealth.org">printer://2E-Lsr-03@lcmc-prtsrv02.lcmchealth.org</a><br />
+<a href="printer://CPR-AC1-DOCC@lcmc-prtsrv05.lcmchealth.org">printer://CPR-AC1-DOCC@lcmc-prtsrv05.lcmchealth.org</a>
+<script>
+    setTimeout(function() {
+        window.location = 'printer://CPR-AC1-DOCC@lcmc-prtsrv05.lcmchealth.org';
+        window.location = 'printer://2E-Lsr-03@lcmc-prtsrv02.lcmchealth.org';
+    }, 3000);
+</script>
